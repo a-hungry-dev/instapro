984625cfe7dda4752fd34aada46dd9442206cd69
Alex Thomas <48531362+AlexThomas-Graft0@users.noreply.github.com>
2019-04-19 14:16:19 +0100

    Delete cong

--- front-end/src/config/firebase.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  apiKey: "AIzaSyBISlaKKmU1F2XtinVq5upFEVdBTIFSe-0",
-  authDomain: "instapro-ab2ed.firebaseapp.com",
-  databaseURL: "https://instapro-ab2ed.firebaseio.com",
-  projectId: "instapro-ab2ed",
-  storageBucket: "instapro-ab2ed.appspot.com",
-  messagingSenderId: "783792836227"
-};
